0567563b8d3e8afd5beec164fddaea7c9c1af5df
Bliss Singhal <54075580+bluelilly1@users.noreply.github.com>
2021-08-07 16:28:40 -0700

    Update and rename lesson7assignment1.java to lesson6assignment1.java

---
+++ lesson6assignment1.java
@@ -0,0 +1,18 @@
+class Main {
+  //you will be making a magic 8 ball simulator where the user types there question in the console and the app gives a random magic 8 ball response
+  
+  //import your libraries here
+
+  //make a method that takes in the question that the user asks as its parameter and returns the magic 8 ball answer
+
+  //make another method that uses a library to get the users question and saves it to some variable
+
+
+  public static void main(String[] args) {
+
+    //make an object and call the methods on that object
+
+    //print out the magic 8 ball's response
+
+  }
+}
